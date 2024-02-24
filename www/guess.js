76465b16ae5fb513e8d41e167d1f73c2449eb5de
mdpadberg <mdpadberg@protonmail.com>
2024-02-24 21:22:59 +0100

    Some frontend changes

--- www/guess.js
+++ www/guess.js
@@ -7,12 +7,16 @@ function get3images(event) {
     event.preventDefault();
     try {
         document.getElementById('error-message-text').innerText = '';
-        document.getElementById('imageone').innerHTML = `<input type="radio" id="one" name="image" value="one" checked />${get_random_image()}`;
-        document.getElementById('imagetwo').innerHTML = `<input type="radio" id="two" name="image" value="two" />${get_random_image()}`;
-        document.getElementById('imagethree').innerHTML = `<input type="radio" id="three" name="image" value="three" />${get_random_image()}`;
+        document.getElementById('imageone').innerHTML = imageElement(true, 'one');
+        document.getElementById('imagetwo').innerHTML = imageElement(false, 'two');
+        document.getElementById('imagethree').innerHTML = imageElement(false, 'three');
     } catch (e) {
         document.getElementById('error-message-text').innerText = e;
     }
+}
+
+function imageElement(checked, id) {
+    return `<input type="radio" id="${id}" name="image" value="${id}" ${checked ? 'checked' : ''}/>${get_random_image()}`
 }
 
 function guessing(event) {
@@ -21,7 +25,10 @@ function guessing(event) {
         document.getElementById('error-message-text').innerText = '';
         const selector = document.querySelector('input[name="image"]:checked');
         if (selector) {
-            const neuralnetwork = document.getElementById("uploadInput").files[0];
+            const neuralnetwork = document.getElementById('uploadInput').files[0];
+            if(!neuralnetwork) {
+                throw new Error("You didn't upload a neural network");
+            }
             const reader = new FileReader();
             reader.readAsText(neuralnetwork);
             reader.onload = (function (f) {
@@ -29,10 +36,10 @@ function guessing(event) {
                     const neuralnetworkAsText = e.target.result;
                     const selectedMnistImage = selector.nextSibling.outerHTML;
                     const result = query_neuralnetwork(neuralnetworkAsText, selectedMnistImage);
-                    const resulttable = document.getElementById("resulttable");
-                    let tabledata = "<tr><td>Number</td><td>%</td></tr>";
+                    const resulttable = document.getElementById('resulttable');
+                    let tabledata = '<tr><td>Number</td><td>%</td></tr>';
                     for (var i = 0; i < result.length; i++) {
-                        tabledata += `<tr><td>${i}</td><td>${result[i]}</td></tr>`;
+                        tabledata += `<tr><td>${i}</td><td>${result[i]*100}</td></tr>`;
                     };
                     resulttable.innerHTML = tabledata;
                 };
